fca902224f1f970939554c34e61b54276a9800fe
abkff <adrien.bikodoroff@gmail.com>
2024-06-29 21:04:55 +0200

    gestion user

---
+++ src/Controller/Admin/adminController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class adminController extends AbstractDashboardController
+{
+    #[Route('/admin', name: 'admin')]
+    public function index(): Response
+    {
+
+         return $this->render('admin/my-dashboard.html.twig');
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('E Commerce');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        return[
+        MenuItem::linkToDashboard('Dashboard', 'fa fa-home'),
+
+            MenuItem::section('light commerce'),
+
+            MenuItem::section('Utilisateur'),
+            MenuItem::linkToCrud('Users', 'fa fa-user', User::class),
+            ];
+    }
+}
